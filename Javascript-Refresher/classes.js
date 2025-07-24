121986c380203c10a3832c2eee4fb4250adde6fd
Nyariki Kevin <138714529+nyrkn10@users.noreply.github.com>
2025-07-24 16:01:31 +0300

    classes,constructor,inheritance

---
+++ Javascript-Refresher/classes.js
@@ -0,0 +1,24 @@
+class Human {
+    constructor(){
+        this.gender = 'male';
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super(); // Call the constructor of the parent class
+        this.name = 'Max';
+    }
+
+    printName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printName();
+person.printGender(); 
